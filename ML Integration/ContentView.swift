cda7039dcaf0001e338fc5e14c99c450b21aaf05
TBDO Inc <tbdoadmin@proton.me>
2026-04-17 11:59:27 -0700

    Initial Commit

---
+++ ML Integration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ML Integration
+//
+//  Created by TBDO Inc on 4/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ML Integration/ML_IntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ML_IntegrationApp.swift
+//  ML Integration
+//
+//  Created by TBDO Inc on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct ML_IntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
